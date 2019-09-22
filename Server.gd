0fa50146f1f611f8985353a536d21cf2184449a4
Mark Achee <mark@achee.com>
2019-09-22 14:52:36 -0500

    add Server ticks, temp at 1s per tick

--- Server.gd
+++ Server.gd
@@ -1,4 +1,20 @@
 extends Node
 
+var delta_total = 0
+var tick = 0
+
 func _ready():
     lug.lug("Server Ready")
+
+func _process(delta):
+    delta_total += delta
+    var ticks = 0
+    
+    while (delta_total > 1):
+        ticks += 1
+        delta_total -= 1
+        lug.lug("processed tick %s" % tick)
+        tick += 1
+
+    if (ticks > 1):
+        lug.lug("more than 1 tick processed in single _process call")
